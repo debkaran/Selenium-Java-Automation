e52e357489fd270bdaa83d4ef092546b6d2d40f5
debkaran <84189775+debkaran@users.noreply.github.com>
2025-07-20 13:02:04 +0530

    Delete project/PageObjectModel directory

--- project/PageObjectModel/src/main/java/com/test/pages/crm/CRMHomePage.java
+++
@@ -1,7 +0,0 @@
-package com.test.pages.crm;
-
-import com.test.base.Page;
-
-public class CRMHomePage extends Page {
-
-}
--- project/PageObjectModel/src/main/java/com/test/pages/crm/accounts/ImportAccountsPage.java
+++
@@ -1,7 +0,0 @@
-package com.test.pages.crm.accounts;
-
-import com.test.base.Page;
-
-public class ImportAccountsPage extends Page {
-
-}
